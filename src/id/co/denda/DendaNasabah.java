b8a4d4075557051047c10dfd697c22ee95168d0d
user <user@DESKTOP-2DR0I7N>
2017-10-02 17:09:02 +0700

    Commit Ketiga

---
+++ src/id/co/denda/DendaNasabah.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.co.denda;
+
+/**
+ *
+ * @author Aris
+ */
+public class DendaNasabah {
+    private double denda;
+
+    /**
+     * @return the denda
+     */
+    public double getDenda() {
+        return denda;
+    }
+
+    /**
+     * @param denda the denda to set
+     */
+    public void setDenda(double denda) {
+        this.denda = denda;
+    }
+}
